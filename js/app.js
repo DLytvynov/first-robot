c219434fbe6ea74b2326321cf78747f183aea990
Dmitriy Lytvynov <dmitriy.lytvynov@gmail.com>
2018-01-26 19:39:04 +0200

    game over

--- js/app.js
+++ js/app.js
@@ -10,6 +10,11 @@ window.onload = function () {
     const BOTTOM_DIR = 'ArrowDown';
 
     let robot = document.querySelector('#img');
+    let mine = document.querySelector('#mine');
+    let mine_x = parseInt(window.getComputedStyle(mine).left);
+    let mine_y = parseInt(window.getComputedStyle(mine).top);
+
+console.log(mine_x, mine_y);
 
     $(document).on('keydown', function (e) {
         //console.log(e.key);
@@ -78,9 +83,31 @@ window.onload = function () {
             robot.style.top = y + 'px';
         }
 
+        //console.log('X = ' + robot.style.left + '; Y = ' + robot.style.top);
 
+        x = parseInt(window.getComputedStyle(robot).left);
+        y = parseInt(window.getComputedStyle(robot).top);
+
+        if ((x > mine_x - 150 && x < mine_x)
+        && (y > mine_y - 150 && y < mine_y))
+        {
+            robot.style.display = 'none';
+            mine.style.display = 'none';
+            boom.style.display = 'block';
+
+            $('#next').css('display','block')
+        }
     }//go
 
+    $('#next').on('click', function () {
+        $(this).css('display','none');
+        robot.style.left = 0;
+        robot.style.top = 0;
+            robot.style.display = 'block';
+        mine.style.display = 'block';
+        boom.style.display = 'none';
+
+    });
 };
 
 
